19371926b9f111152a246b56ffeef135faa44bd8
Eric Barrett <ericnbarrett@gmail.com>
2012-03-07 14:12:00 +0400

    Added basic multilingual controller, views switcher, and routing

--- config/routes.rb
+++ config/routes.rb
@@ -16,12 +16,18 @@ GermanWeek::Application.routes.draw do
 
   resources :tags
 
-  resources :event_sponsors
-
-  resources :sponsors
-
-  resources :events
-
+  scope "/:locale" do
+    resources :event_sponsors
+  end
+  
+  scope "/:locale" do
+    resources :sponsors
+  end
+  
+  scope "/:locale" do
+    resources :events
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -71,6 +77,8 @@ GermanWeek::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+  
+   match '/:locale' => "german_week#index"
    root :to => 'german_week#index'
 
   # See how all your routes lay out with "rake routes"
